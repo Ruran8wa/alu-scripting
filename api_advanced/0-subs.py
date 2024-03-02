9169c7011a456d1287dd2cb879b5cd6b4709ec49
ruran8wa <p.rurangwa@alustudent.com>
2024-03-02 09:25:44 -0800

    errors fix

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-"""Return number of subscribers for a given subreddit"""
+"""Return the number of subscribers of a given subreddit"""
+
 import requests
 
+
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    
+    """function that fetches number_of_subscribers"""
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+
     try:
-        response = requests.get(url, headers=headers)
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    except (requests.RequestException, KeyError):
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        return RESPONSE.json().get("data").get("subscribers")
+
+    except Exception:
         return 0
-
-# Example usage:
-subreddit_name = "learnpython"
-subscribers = number_of_subscribers(subreddit_name)
-if subscribers != 0:
-    print(f"The subreddit r/{subreddit_name} has {subscribers} subscribers.")
-else:
-    print(f"The subreddit r/{subreddit_name} is invalid or does not exist.")
-
