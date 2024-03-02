97afc5c15f076872b421886301d82c4f68554c14
ruran8wa <p.rurangwa@alustudent.com>
2024-03-02 09:02:25 -0800

    errors fix

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
-"""Return the number of subscribers of a given subreddit"""
-
+"""Return number of subscribers for a given subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """function that fetches number_of_subscribers"""
-    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
-
-    try:
-        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        if RESPONSE.status_code == 200:
-            return RESPONSE.json().get("data").get("subscribers")
-        else:
-            return 0
-
-    except requests.exceptions.RequestException as e:
-        print("Error:", e)
-        return 0
-    except Exception as e:
-        print("Unexpected error:", e)
-        return 0
-
+    """Return the number of subscribers """
+    url = "https://www.reddit.com/r/{}/about.json" \
+        .format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json().get('data') \
+            .get('subscribers')
+    return 0
